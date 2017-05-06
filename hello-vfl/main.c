d4b2008505e078b273ff753320bf6908a6380bce
Nat! <nat@mulle-kybernetik.com>
2017-05-06 16:13:18 +0200

    Initial commit

---
+++ hello-vfl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-vfl
+//
+//  Created by Nat! on 06.05.17.
+//  Copyright © 2017 Mulle kybernetiK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, VfL Bochum!\n");
+    return 0;
+}
